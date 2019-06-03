732dbbfb7df3c4d650876ffd10d141aa31f4a47f
victorfence <victor@education2000.com>
2019-06-03 09:25:56 +0800

    Update text

--- background.js
+++ background.js
@@ -79,7 +79,7 @@ function startWebServer(port){
                 }else{
                     var version = r.Version || 'unknown'
                     backend_version = version;
-                    log.info(`backend service started, version = ${version} (wanted = 1.7.0)`)
+                    log.info(`backend service started, version = ${version} (wanted >= 1.7.0)`)
                     web_started = true;
                     browser.runtime.sendMessage({type: 'BACKEND_SERVICE_STARTED', version});
                     resolve();
