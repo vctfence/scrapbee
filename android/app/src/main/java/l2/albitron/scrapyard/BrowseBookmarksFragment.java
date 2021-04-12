0e189b05dba31119920983cd4c27bc089562cf1b
GChristensen <gchristnsn@gmail.com>
2021-04-12 22:22:28 +0400

    implemented cloud shelf bookmark browsing in android application

--- android/app/src/main/java/l2/albitron/scrapyard/BrowseBookmarksFragment.java
+++ android/app/src/main/java/l2/albitron/scrapyard/BrowseBookmarksFragment.java
@@ -12,6 +12,7 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,51 +20,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import l2.albitron.scrapyard.cloud.DropboxProvider;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BrowseBookmarksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BrowseBookmarksFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public BrowseBookmarksFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BrowseBookmarksFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BrowseBookmarksFragment newInstance(String param1, String param2) {
-        BrowseBookmarksFragment fragment = new BrowseBookmarksFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new BrowseBookmarksFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -83,7 +52,7 @@ public class BrowseBookmarksFragment extends Fragment {
             public void onPageFinished(WebView view, String url) {
                 ExecutorService executor = Executors.newSingleThreadExecutor();
                 Handler handler = new Handler(Looper.getMainLooper());
-
+                //System.out.println(view.getSettings().getUserAgentString());
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
