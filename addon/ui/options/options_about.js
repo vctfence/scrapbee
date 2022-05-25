f2a2279225bedc5d32066aaec5cc4ef1daa1285c
GChristensen <gchristnsn@gmail.com>
2022-05-25 23:57:05 +0400

    added new donation options

--- addon/ui/options/options_about.js
+++ addon/ui/options/options_about.js
@@ -3,4 +3,7 @@ import {fetchText} from "../../utils_io.js";
 export async function load() {
     $("#about-changes").html(await fetchText("options/options_changes.html"));
     $("#about-version").text(`Version: ${browser.runtime.getManifest().version}`);
+
+    $("#donation-links").on("mouseenter", e => $("#scrapyard-logo").prop("src", "../images/donation_kitty.png"));
+    $("#donation-links").on("mouseleave", e => $("#scrapyard-logo").prop("src", "../icons/scrapyard.svg"));
 }
